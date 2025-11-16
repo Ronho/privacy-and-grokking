8e705b28b9b80e7114362b6da40cd10d4cd3f12d
Ron Holzapfel <ron.holzapfel@deutschebahn.com>
2025-11-16 09:00:33 +0100

    chore: name layers for easier identification within rdms

--- src/privacy_and_grokking/models/cnn.py
+++ src/privacy_and_grokking/models/cnn.py
@@ -1,22 +1,24 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.pool = nn.MaxPool2d(2, 2)
-        self.model = nn.Sequential(
-            nn.Conv2d(1, 6, 3),
-            nn.ReLU(),
-            self.pool,
-            nn.Conv2d(6, 16, 3),
-            nn.ReLU(),
-            self.pool,
-            nn.Flatten(),
-            nn.Linear(400, 200),
-            nn.ReLU(),
-            nn.Linear(200, 10),
-        )
-        
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 5 * 5, 200)
+        self.fc2 = nn.Linear(200, 10)  
 
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, input):
+        y = self.conv1(input)
+        y = F.relu(y)
+        y = F.max_pool2d(y, 2, 2)
+        y = self.conv2(y)
+        y = F.relu(y)
+        y = F.max_pool2d(y, 2, 2)
+        y = torch.flatten(y, 1)
+        y = self.fc1(y)
+        y = F.relu(y)
+        y = self.fc2(y)
+        return y
--- src/privacy_and_grokking/models/mlp.py
+++ src/privacy_and_grokking/models/mlp.py
@@ -1,16 +1,19 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class MLP(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(784, 200),
-            nn.ReLU(),
-            nn.Linear(200, 200),
-            nn.ReLU(),
-            nn.Linear(200, 10)
-        )
+        self.fc1 = nn.Linear(28 * 28, 200)
+        self.fc2 = nn.Linear(200, 200)
+        self.fc3 = nn.Linear(200, 10)
 
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, input):
+        y = torch.flatten(input, 1)
+        y = self.fc1(y)
+        y = F.relu(y)
+        y = self.fc2(y)
+        y = F.relu(y)
+        y = self.fc3(y)
+        return y
