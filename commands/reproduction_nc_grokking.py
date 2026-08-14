e257b5edf33c344c3de7386e5aa67445e1a70679
Ronho <ronholzapfel@outlook.de>
2026-08-14 15:09:23 +0200

    update

--- commands/reproduction_nc_grokking.py
+++ commands/reproduction_nc_grokking.py
@@ -18,7 +18,7 @@ configs_list = list(configs.glob("*.json"))
 configs_list = [c for c in configs_list if not c.name.startswith("_")]
 
 def cmd(config, seed, name_prefix="", postfix=None, ):
-    cmd_str = f"uv run pag train reproduction-nc-grokking {config.name} 150000 --run-name {name_prefix}{config.stem} -o seed={seed} -o data.seed={seed}"
+    cmd_str = f"pag train reproduction-nc-grokking {config.name} 150000 --run-name {name_prefix}{config.stem} -o seed={seed} -o data.seed={seed}"
     if load_all_to_gpu:
         cmd_str += " --load-all-to-gpu"
     if postfix is not None:
