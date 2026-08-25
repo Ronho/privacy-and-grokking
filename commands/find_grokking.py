62c0d052a75fbda2ecb3d9d3ac5531eee72911d9
Ronho <ronholzapfel@outlook.de>
2026-08-25 18:27:23 +0200

    update

--- commands/find_grokking.py
+++ commands/find_grokking.py
@@ -15,7 +15,8 @@ seed=4713
 
 initialization_scale = [2.0, 3.0, 6.0, 9.0, 12.0]
 weight_decay = [1.0, 0.5, 0.1, 0.05, 0.01, 0.001, 0.0001]
-train_size = [10_000, 5_000, 2_000, 1_000, 500]
+# train_size = [10_000, 5_000, 2_000, 1_000, 500]
+train_size = [None, 10_000, 5_000, 2_000, 1_000, 500]
 
 
 # Start of main script
@@ -24,20 +25,25 @@ permutations = list(itertools.product(initialization_scale, weight_decay, train_
 
 lines = []
 configs_list = list(configs.glob("*.json"))
-configs_list = [c for c in configs_list if not c.name.startswith("_") and not c.name.startswith("+")]
+configs_list = [c for c in configs_list if c.name.startswith("-")]
 
 def cmd(config, seed, name_prefix="", postfix=None):
-    cmd_str = f"pag train find-grokking {config.name} 150000 --run-name {name_prefix}{config.stem} -o seed={seed} -o data.seed={seed}"
+    cmd_str = f"pag train find-grokking {config.name} 150000 --run-name {name_prefix}{config.stem[1:]} -o seed={seed} -o data.seed=4711"
     if load_all_to_gpu:
         cmd_str += " --load-all-to-gpu"
     if postfix is not None:
         cmd_str += postfix
     return cmd_str
 
+# for config in configs_list:
+#     for scale, decay, size in permutations:
+#         seed = random.randint(0, 1000000)
+#         lines.append(cmd(config, seed, postfix=f" -o data.mask.seed=4711 -o model.initialization_scale={scale} -o optimizer.weight_decay={decay} -o data.train_size={size}"))
 for config in configs_list:
-    for scale, decay, size in permutations:
-        seed = random.randint(0, 1000000)
-        lines.append(cmd(config, seed, postfix=f" -o data.mask.seed={seed} -o model.initialization_scale={scale} -o optimizer.weight_decay={decay} -o data.train_size={size}"))
+    for (key, val) in [("model.initialization_scale=", initialization_scale), ("optimizer.weight_decay=", weight_decay), ("data.train_size=", train_size)]:
+        for v in val:
+            seed = random.randint(0, 1000000)
+            lines.append(cmd(config, seed, postfix=f" -o data.mask.seed=4711 -o {key}{v}"))
 
 if shuffle:
     random.shuffle(lines)
